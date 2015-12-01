888b3bcf88b192c902f2969b58945563b401f545
Mark Struck <Mark Struck>
2015-12-01 03:30:52 -0600

    Added clean-css, bundled app. converted pile to ES6 class.

--- app/config.js
+++ app/config.js
@@ -13,6 +13,40 @@ System.config({
     "npm:*": "jspm_packages/npm/*",
     "jspm:*": "jspm_packages/jspm/*"
   },
+  bundles: {
+    "app.bundle.js": [
+      "app.js",
+      "github:angular/bower-angular@1.4.8",
+      "github:fatlinesofcode/ngDraggable@0.1.8",
+      "klondike/klondike.js",
+      "klondike/game.js",
+      "klondike/scoring.js",
+      "klondike/board.js",
+      "github:fatlinesofcode/ngDraggable@0.1.8/ngDraggable",
+      "klondike/game.css!github:systemjs/plugin-css@0.1.20",
+      "github:angular/bower-angular@1.4.8/angular",
+      "npm:underscore@1.8.3",
+      "klondike/piles/tableauPile.js",
+      "jspm:cards@dev",
+      "klondike/piles/remainderPile.js",
+      "klondike/piles/foundationPile.js",
+      "npm:babel-runtime@5.8.34/helpers/class-call-check",
+      "npm:babel-runtime@5.8.34/helpers/create-class",
+      "github:angular/bower-angular-route@1.4.8",
+      "npm:underscore@1.8.3/underscore",
+      "jspm:cards@dev/index",
+      "npm:babel-runtime@5.8.34/core-js/object/create",
+      "klondike/piles/pile.js",
+      "npm:babel-runtime@5.8.34/core-js/object/define-property",
+      "github:angular/bower-angular-route@1.4.8/angular-route",
+      "npm:core-js@1.2.6/library/fn/object/define-property",
+      "jspm:cards@dev/src/card",
+      "jspm:cards@dev/src/deck",
+      "npm:core-js@1.2.6/library/fn/object/create",
+      "npm:core-js@1.2.6/library/modules/$"
+    ]
+  },
+
   map: {
     "angular": "github:angular/bower-angular@1.4.8",
     "angular-mocks": "github:angular/bower-angular-mocks@1.4.8",
@@ -21,6 +55,7 @@ System.config({
     "babel-runtime": "npm:babel-runtime@5.8.34",
     "card": "jspm:cards@dev/src/card.js",
     "cards": "jspm:cards@dev",
+    "clean-css": "npm:clean-css@3.4.8",
     "core-js": "npm:core-js@1.2.6",
     "css": "github:systemjs/plugin-css@0.1.20",
     "deck": "jspm:cards@dev/src/deck.js",
@@ -40,11 +75,37 @@ System.config({
     "github:jspm/nodelibs-assert@0.1.0": {
       "assert": "npm:assert@1.3.0"
     },
+    "github:jspm/nodelibs-buffer@0.1.0": {
+      "buffer": "npm:buffer@3.5.3"
+    },
+    "github:jspm/nodelibs-events@0.1.1": {
+      "events": "npm:events@1.0.2"
+    },
+    "github:jspm/nodelibs-http@1.7.1": {
+      "Base64": "npm:Base64@0.2.1",
+      "events": "github:jspm/nodelibs-events@0.1.1",
+      "inherits": "npm:inherits@2.0.1",
+      "stream": "github:jspm/nodelibs-stream@0.1.0",
+      "url": "github:jspm/nodelibs-url@0.1.0",
+      "util": "github:jspm/nodelibs-util@0.1.0"
+    },
+    "github:jspm/nodelibs-https@0.1.0": {
+      "https-browserify": "npm:https-browserify@0.0.0"
+    },
+    "github:jspm/nodelibs-os@0.1.0": {
+      "os-browserify": "npm:os-browserify@0.1.2"
+    },
     "github:jspm/nodelibs-path@0.1.0": {
       "path-browserify": "npm:path-browserify@0.0.0"
     },
     "github:jspm/nodelibs-process@0.1.2": {
       "process": "npm:process@0.11.2"
+    },
+    "github:jspm/nodelibs-stream@0.1.0": {
+      "stream-browserify": "npm:stream-browserify@1.0.0"
+    },
+    "github:jspm/nodelibs-url@0.1.0": {
+      "url": "npm:url@0.10.3"
     },
     "github:jspm/nodelibs-util@0.1.0": {
       "util": "npm:util@0.10.3"
@@ -52,10 +113,45 @@ System.config({
     "jspm:cards@dev": {
       "underscore": "npm:underscore@1.8.3"
     },
+    "npm:amdefine@1.0.0": {
+      "fs": "github:jspm/nodelibs-fs@0.1.2",
+      "module": "github:jspm/nodelibs-module@0.1.0",
+      "path": "github:jspm/nodelibs-path@0.1.0",
+      "process": "github:jspm/nodelibs-process@0.1.2"
+    },
     "npm:assert@1.3.0": {
       "util": "npm:util@0.10.3"
     },
     "npm:babel-runtime@5.8.34": {
+      "process": "github:jspm/nodelibs-process@0.1.2"
+    },
+    "npm:buffer@3.5.3": {
+      "base64-js": "npm:base64-js@0.0.8",
+      "child_process": "github:jspm/nodelibs-child_process@0.1.0",
+      "fs": "github:jspm/nodelibs-fs@0.1.2",
+      "ieee754": "npm:ieee754@1.1.6",
+      "is-array": "npm:is-array@1.0.1",
+      "process": "github:jspm/nodelibs-process@0.1.2"
+    },
+    "npm:clean-css@3.4.8": {
+      "buffer": "github:jspm/nodelibs-buffer@0.1.0",
+      "commander": "npm:commander@2.8.1",
+      "fs": "github:jspm/nodelibs-fs@0.1.2",
+      "http": "github:jspm/nodelibs-http@1.7.1",
+      "https": "github:jspm/nodelibs-https@0.1.0",
+      "os": "github:jspm/nodelibs-os@0.1.0",
+      "path": "github:jspm/nodelibs-path@0.1.0",
+      "process": "github:jspm/nodelibs-process@0.1.2",
+      "source-map": "npm:source-map@0.4.4",
+      "url": "github:jspm/nodelibs-url@0.1.0",
+      "util": "github:jspm/nodelibs-util@0.1.0"
+    },
+    "npm:commander@2.8.1": {
+      "child_process": "github:jspm/nodelibs-child_process@0.1.0",
+      "events": "github:jspm/nodelibs-events@0.1.1",
+      "fs": "github:jspm/nodelibs-fs@0.1.2",
+      "graceful-readlink": "npm:graceful-readlink@1.0.1",
+      "path": "github:jspm/nodelibs-path@0.1.0",
       "process": "github:jspm/nodelibs-process@0.1.2"
     },
     "npm:core-js@1.2.6": {
@@ -64,14 +160,57 @@ System.config({
       "process": "github:jspm/nodelibs-process@0.1.2",
       "systemjs-json": "github:systemjs/plugin-json@0.1.0"
     },
+    "npm:core-util-is@1.0.2": {
+      "buffer": "github:jspm/nodelibs-buffer@0.1.0"
+    },
+    "npm:graceful-readlink@1.0.1": {
+      "fs": "github:jspm/nodelibs-fs@0.1.2"
+    },
+    "npm:https-browserify@0.0.0": {
+      "http": "github:jspm/nodelibs-http@1.7.1"
+    },
     "npm:inherits@2.0.1": {
       "util": "github:jspm/nodelibs-util@0.1.0"
+    },
+    "npm:os-browserify@0.1.2": {
+      "os": "github:jspm/nodelibs-os@0.1.0"
     },
     "npm:path-browserify@0.0.0": {
       "process": "github:jspm/nodelibs-process@0.1.2"
     },
     "npm:process@0.11.2": {
       "assert": "github:jspm/nodelibs-assert@0.1.0"
+    },
+    "npm:punycode@1.3.2": {
+      "process": "github:jspm/nodelibs-process@0.1.2"
+    },
+    "npm:readable-stream@1.1.13": {
+      "buffer": "github:jspm/nodelibs-buffer@0.1.0",
+      "core-util-is": "npm:core-util-is@1.0.2",
+      "events": "github:jspm/nodelibs-events@0.1.1",
+      "inherits": "npm:inherits@2.0.1",
+      "isarray": "npm:isarray@0.0.1",
+      "process": "github:jspm/nodelibs-process@0.1.2",
+      "stream-browserify": "npm:stream-browserify@1.0.0",
+      "string_decoder": "npm:string_decoder@0.10.31"
+    },
+    "npm:source-map@0.4.4": {
+      "amdefine": "npm:amdefine@1.0.0",
+      "process": "github:jspm/nodelibs-process@0.1.2"
+    },
+    "npm:stream-browserify@1.0.0": {
+      "events": "github:jspm/nodelibs-events@0.1.1",
+      "inherits": "npm:inherits@2.0.1",
+      "readable-stream": "npm:readable-stream@1.1.13"
+    },
+    "npm:string_decoder@0.10.31": {
+      "buffer": "github:jspm/nodelibs-buffer@0.1.0"
+    },
+    "npm:url@0.10.3": {
+      "assert": "github:jspm/nodelibs-assert@0.1.0",
+      "punycode": "npm:punycode@1.3.2",
+      "querystring": "npm:querystring@0.2.0",
+      "util": "github:jspm/nodelibs-util@0.1.0"
     },
     "npm:util@0.10.3": {
       "inherits": "npm:inherits@2.0.1",
--- app/klondike/piles/pile.js
+++ app/klondike/piles/pile.js
@@ -1,28 +1,36 @@
-export default function Pile(cards, scoring) {
-  this.cards = cards || [];
-  this.scoring = scoring;
+export default class Pile {
+  
+  constructor(cards, scoring) {
+    this.cards = cards || [];
+    this.scoring = scoring;
+  }
+  
+  isEmpty() {
+    return this.cards.length === 0;
+  };
+  
+  topCard() {
+    return this.cards[this.cards.length - 1];
+  };
+
+  removeCard(card) {
+    var index = this.cards.indexOf(card);
+    this.cards.splice(index, 1);
+  };
+
+  addTopCard(card) {
+    this.cards.push(card);
+  };
+
+  moveTopCardFrom(source) {
+    var topOfSource = source.topCard();
+    var accepted = this.drop(topOfSource);
+    if (accepted) {
+      this.scoring.dropped(source.constructor, this.constructor);
+      source.removeCard(topOfSource);
+    }
+  }
 }
 
-Pile.prototype.isEmpty = function () {
-  return this.cards.length === 0;
-};
-Pile.prototype.topCard = function () {
-  return this.cards[this.cards.length - 1];
-};
-Pile.prototype.removeCard = function (card) {
-  var index = this.cards.indexOf(card);
-  this.cards.splice(index, 1);
-};
-Pile.prototype.addTopCard = function (card) {
-  this.cards.push(card);
-};
-Pile.prototype.moveTopCardFrom = function (source) {
-  var topOfSource = source.topCard();
-  var accepted = this.drop(topOfSource);
-  if (accepted) {
-    this.scoring.dropped(source.constructor, this.constructor);
-    source.removeCard(topOfSource);
-  }
-};
 Pile.prototype.moveCardsFrom = Pile.prototype.moveTopCardFrom;
 Pile.increasingRanks = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"];
